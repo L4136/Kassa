6abf4433441c750944c44a4f8eb3f7e4540265b5
Heli Vepsäläinen <L4136@student.jamk.fi>
2018-02-18 19:19:14 +0200

    T8 done

---
+++ ITransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kassa
+{
+    
+    public interface ITransaction
+    {
+        // interface members 
+        string ShowTransaction(float Money);
+        float GetAmount();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,24 @@ namespace Kassa
     {
         static void Main(string[] args)
         {
+            //card
+            PaidWithCard paidWithCard = new PaidWithCard();
+            paidWithCard.ShowTransaction(78.95f);
+            paidWithCard.ShowTransaction(45.65f);
+            paidWithCard.GetAmount();
+
+            //cash
+            PaidWithCash paidWithCash = new PaidWithCash();
+            paidWithCash.ShowTransaction(100);
+            paidWithCash.ShowTransaction(50);
+            paidWithCash.GetAmount();
+
+            //sum
+            float card = paidWithCard.Money;
+            float cash = paidWithCash.Money;
+            float sum = card + cash;
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Total sales {0} is {1}", date, sum);
         }
     }
 }
